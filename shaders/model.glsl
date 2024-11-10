9bd71dda9111d3cb7e8010cead49e558d9a835e4
Allocated Artist <Senorsiete2006@gmail.com>
2024-11-10 13:22:30 -0800

    shader update

--- shaders/model.glsl
+++ shaders/model.glsl
@@ -1,11 +1,8 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
-uniform float size;
-uniform mat4 rotation;
-
 void main() {
-    gl_Position = rotation * vec4(aPos * size, 1.0);
+    gl_Position = vec4(aPos, 1.0);
 }
 #SPLIT
 #version 330 core
