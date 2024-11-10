496e39274e92ecf080531a282133169bb3e3ca1d
Allocated Artist <Senorsiete2006@gmail.com>
2024-11-10 14:27:12 -0800

    shaders

--- shaders/model.glsl
+++ shaders/model.glsl
@@ -1,13 +1,22 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexCoords;
+
+out vec2 fragTexCoords;
 
 void main() {
+    fragTexCoords = aTexCoords;
     gl_Position = vec4(aPos, 1.0);
 }
 #SPLIT
 #version 330 core
 out vec4 fragColor;
 
+in vec2 fragTexCoords;
+
+uniform sampler2D texture0;
+
 void main() {
-    fragColor = vec4(0.9, 0.0, 0.0, 1.0);
+    vec4 texture_color = texture(texture0, fragTexCoords);
+    fragColor = texture_color;
 }
