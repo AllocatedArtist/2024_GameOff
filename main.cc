b70458b7a669b0709f826e4d0b8196b327ef59cd
Allocated Artist <Senorsiete2006@gmail.com>
2024-11-12 15:11:31 -0800

    display pose

--- main.cc
+++ main.cc
@@ -7,6 +7,14 @@
 #include "App.h"
 #include "Graphics.h"
 
+void ProcessRoot(std::vector<glm::mat4>& transforms, Joint root, glm::mat4 parent) {
+  glm::mat4 global = parent * root.transform_;
+  transforms.emplace_back(global);
+  for (const Joint& child : root.children_) {
+    ProcessRoot(transforms, child, global);
+  }
+}
+
 int main(void) {
 
   App app(1600, 1480, "Graphics");
@@ -16,39 +24,47 @@ int main(void) {
   Shader shader;
   shader.LoadShader("../shaders/model.glsl");
 
-  Primitive triangle = Graphics::CreatePrimitive(
-    { 
-      Vertex { glm::vec3(-1.0, 1.0, 0.0), glm::vec2(0.0, 1.0) }, 
-      Vertex { glm::vec3(-1.0, -1.0, 0.0), glm::vec2(0.0, 0.0) }, 
-      Vertex { glm::vec3(1.0, -1.0, 0.0), glm::vec2(1.0, 0.0) },
-      Vertex { glm::vec3(1.0, 1.0, 0.0), glm::vec2(1.0, 1.0) }
-    },
-    {
-      0, 1, 2, 
-      2, 3, 0  
-    }
-  );
+  Model cube;
+  cube.Load("../assets/robot.glb");
 
   InputManager& input = app.GetInputManager();
   input.AddAction(Key::kKeyEscape, "Quit");
   input.AddAction(Key::kKey6, "Quit");
-
-  input.AddAction(Key::kKey0, "t0");
-  input.AddAction(Key::kKey1, "t1");
 
   input.RegisterInputs();
 
   float dt = 1.f / 60.f;
   float accumulator = 0.f;
 
-  Texture shrek;
-  shrek.LoadFromFile("../assets/shrek_map.png", true);
-
+  int32_t u_model = shader.GetUniformLocation("u_Model");
+  int32_t u_vp = shader.GetUniformLocation("u_ViewProjection");  
   int32_t u_texture0 = shader.GetUniformLocation("texture0");
+  int32_t u_base_color = shader.GetUniformLocation("u_baseColor");
 
   shader.Enable();
   shader.SetUniformInt(u_texture0, 0);
   shader.Disable();
+
+  glm::mat4 model(1.0);
+  glm::mat4 view(1.0);
+  glm::mat4 projection(1.0);
+
+  std::vector<int32_t> u_bind_pose;
+  std::vector<glm::mat4> bind_poses;
+  
+  for (int i = 0; i < 100; ++i) {
+    std::string uniform = "u_Joints[" + std::to_string(i) + "]";
+    u_bind_pose.push_back(shader.GetUniformLocation(uniform.c_str()));
+  }
+
+      
+  for (const Skin& skin : cube.GetSkins()) {
+    std::vector<glm::mat4> bone_transforms;
+    ProcessRoot(bone_transforms, skin.root_, glm::mat4(1.0));
+    for (int i = 0; i < skin.inverse_bind_matrices_.size(); ++i) {            
+      bind_poses.push_back(bone_transforms[i] * skin.inverse_bind_matrices_[i]);
+    }
+  }
     
   while (app.Update()) {        
     float frame_time = app.GetDeltaTime();
@@ -59,6 +75,22 @@ int main(void) {
         app.CloseWindow();
       }          
 
+      float t = app.GetTime();
+      float x = cosf(t) * 1.5;
+      float z = sinf(t) * 1.5;
+
+      model = glm::translate(glm::mat4(1.0), glm::vec3(0.0, -1, 0.0));
+      // model = glm::rotate(model, glm::radians(90.f),glm::vec3(1.0,0.0,0.0));
+      // model = glm::scale(model, glm::vec3(0.05));
+      view = glm::lookAt(glm::vec3(x, 0.0, z), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+
+      int32_t width = app.GetScreenWidth();
+      int32_t height = app.GetScreenHeight();
+
+      if (height == 0) height = 1;
+    
+      projection = glm::perspective(glm::radians(90.f), float(width) / float(height), 0.01f, 100.f);
+      
       accumulator -= dt;
     }
 
@@ -67,19 +99,33 @@ int main(void) {
     Graphics::ClearColor(better_white);
 
     shader.Enable();
-    shrek.Bind(0);
+
+    shader.SetUniformMatrix(u_vp, projection * view);
+
+    for (int32_t i = 0; i < bind_poses.size(); ++i) {
+      shader.SetUniformMatrix(u_bind_pose[i], bind_poses[i]);
+    }
+
+    for (const Mesh& mesh : cube.GetMeshes()) {
+      shader.SetUniformMatrix(u_model, model * mesh.local_transform_);
+      for (const MeshPrimitive& primitive : mesh.mesh_primitives_) {
+        if (primitive.material_) {
+          const Material& material = primitive.material_.value();
+          if (material.has_texture_) {
+            Texture(material.texture_id_).Bind(0);
+          }
+          shader.SetUniformVec4(u_base_color, material.color_);
+        }        
+        Graphics::RenderPrimitiveIndexed(primitive.primitive_);
+        Texture(0).Unbind();
+      }
+    }
     
-    Graphics::RenderPrimitiveIndexed(triangle);
-    
-    shrek.Unbind();
     shader.Disable();
     
     app.EndFrame();    
   }
-
-  shrek.UnloadTexture();
   
-  Graphics::DestroyPrimitive(triangle);
   shader.UnloadShader();
   
   return 0;
